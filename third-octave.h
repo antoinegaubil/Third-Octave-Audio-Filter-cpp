e469ead371c4250e78293443d7c88c7256abd073
Antoine Gaubil <90474617+antoinegaubil@users.noreply.github.com>
2022-05-15 15:53:17 -0400

    Update third-octave.h

--- third-octave.h
+++ third-octave.h
@@ -15,8 +15,8 @@
 #include <stdlib.h>
 
 //Githubs
-#include "src/coeff-butter.h"
-//#include "src/coeff-cheby2"
+#include "src/coeff-butter.h" //Comment "#include "src/coeff-cheby2"" to use ButterWorth.
+//#include "src/coeff-cheby2" //Comment "#include "src/coeff-butter.h"" to use chebyshev II.
 #include "src/FiltFilt.h"
 #include "src/downsample.h"
 
