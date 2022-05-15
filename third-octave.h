80e7033e491bd42d5211ef345a31e2f666c4a5e0
Antoine Gaubil <90474617+antoinegaubil@users.noreply.github.com>
2022-05-15 16:17:22 -0400

    Update third-octave.h

--- third-octave.h
+++ third-octave.h
@@ -40,6 +40,7 @@ int third_octave(vector<double> wavedata, double F[], float samplingrate);
 int third_octave(vector<double> wavedata, double F[], float samplingrate)
 {
     vector<double> data(samplingrate);
+    FrequencyTable[33] = {12.5,16,20,25.0, 31.5, 40.0, 50.0, 63.0, 80.0, 100.0, 125.0, 160.0, 200.0, 250.0, 315.0, 400.0, 500.0, 630.0, 800.0, 1000.0,1250.0, 1600.0, 2000.0, 2500.0, 3150.0, 4000.0, 5000.0, 6300.0, 8000.0, 10000.0, 12500.0, 16000.0, 20000.0};
     
     for (int k=0; k<samplingrate; k++)
     {
